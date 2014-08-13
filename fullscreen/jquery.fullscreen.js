91f4fe01400ef9b79c43e53d3c67b56a6e12c19e
James Sulak <jsulak@gmail.com>
2014-08-13 08:32:01 -0500

    Updated plugin to fullscreen selected element without inserting a wrapper div.
    This makes full-screening the html element possible.

--- fullscreen/jquery.fullscreen.js
+++ fullscreen/jquery.fullscreen.js
@@ -1,6 +1,6 @@
 /**
  * @name        jQuery FullScreen Plugin
- * @author      Martin Angelov, Morten Sjøgren
+ * @author      Martin Angelov, Morten Sjøgren, James Sulak
  * @version     1.2
  * @url         http://tutorialzine.com/2012/02/enhance-your-website-fullscreen-api/
  * @license     MIT License
@@ -10,7 +10,6 @@
 (function($){
 	"use strict";
 
-	// These helper functions available only to our plugin scope.
 	function supportFullScreen(){
 		var doc = document.documentElement;
 
@@ -25,7 +24,7 @@
 		} else if (elem.mozRequestFullScreen) {
 			elem.mozRequestFullScreen();
 		} else if (elem.webkitRequestFullScreen) {
-			elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+			elem.webkitRequestFullScreen();
 		}
 	}
 
@@ -66,52 +65,24 @@
 			return this;
 		}
 
-		if(fullScreenStatus()){
+		if (fullScreenStatus()){
 			// if we are already in fullscreen, exit
 			cancelFullScreen();
 			return this;
 		}
 
-		// You can potentially pas two arguments a color
-		// for the background and a callback function
-
 		var options = $.extend({
-			'background'      : '#111',
 			'callback'        : $.noop( ),
 			'fullscreenClass' : 'fullScreen'
 		}, props),
 
-		elem = this,
-
-		// This temporary div is the element that is
-		// actually going to be enlarged in full screen
-
-		fs = $('<div>', {
-			'css' : {
-				'overflow-y' : 'auto',
-				'background' : options.background,
-				'width'      : '100%',
-				'height'     : '100%'
-			}
-		})
-			.insertBefore(elem)
-			.append(elem);
+		elem = this;
 
 		// You can use the .fullScreen class to
 		// apply styling to your element
 		elem.addClass( options.fullscreenClass );
 
-		// Inserting our element in the temporary
-		// div, after which we zoom it in fullscreen
-
-		requestFullScreen(fs.get(0));
-
-		fs.click(function(e){
-			if(e.target == this){
-				// If the black bar was clicked
-				cancelFullScreen();
-			}
-		});
+		requestFullScreen(elem.get(0));
 
 		elem.cancel = function(){
 			cancelFullScreen();
@@ -121,19 +92,14 @@
 		onFullScreenEvent(function(fullScreen){
 			if(!fullScreen){
 				// We have exited full screen.
-			        // Detach event listener
-			        $(document).off( 'fullscreenchange mozfullscreenchange webkitfullscreenchange' );
-				// Remove the class and destroy
-				// the temporary div
-
-				elem.removeClass( options.fullscreenClass ).insertBefore(fs);
-				fs.remove();
+				$(document).off( 'fullscreenchange mozfullscreenchange webkitfullscreenchange' );
+				elem.removeClass( options.fullscreenClass );
 			}
 
-			// Calling the facultative user supplied callback
+			// Calling the user-supplied callback
 			if(options.callback) {
-                            options.callback(fullScreen);
-                        }
+				options.callback(fullScreen);
+			}
 		});
 
 		return elem;
@@ -141,7 +107,6 @@
 
 	$.fn.cancelFullScreen = function( ) {
 			cancelFullScreen();
-
 			return this;
 	};
 }(jQuery));
